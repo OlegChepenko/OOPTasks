160fcd09b80fbfa03a85f018cbee230739ab54cc
Oleg Chepenko <7613332@gmail.com>
2023-05-26 23:05:25 +0300

    26.05.2023

---
+++ src/delete/Month.java
@@ -0,0 +1,16 @@
+package delete;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
